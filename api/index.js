45d9a66abc785fd5bfec145467c67c763740b687
Shibu Joseph <shibujoseph1815@gmail.com>
2024-08-29 22:43:00 +0530

    create mongodb database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
 const port = 3000;
+
+mongoose
+    .connect(process.env.MONGODB_RESOURCE)
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((error) => {
+        console.log(`Database Connection-${error}`);
+    });
+
+
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}!!`);
