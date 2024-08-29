7565652707fc8454f1b9b421bbb87377f3288de1
Shibu Joseph <shibujoseph1815@gmail.com>
2024-08-29 14:02:47 +0530

    create header component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,14 @@ import Dashboard from './pages/Dashboard';
 import Project from './pages/Project';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
+        <Route path="/" element={<Home />} />
         <Route path='/Home'  element={<Home />}/>
         <Route path='/About'  element={<About />}/>
         <Route path='/Dashboard'  element={<Dashboard />}/>
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,8 +1,13 @@
+// tailwind.config.js
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+import flowbite from "flowbite-react/tailwind";
+
+export default {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', 
+    flowbite.content()
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [flowbite.plugin()],
+};
